4db618a99f47367db9adef283fb36d92a3f19f4e
Gabriel Koyamao <>
2018-09-24 10:05:48 -0300

    x

--- js/myp5.js
+++ js/myp5.js
@@ -1,18 +1,33 @@
+var bird;
+var pipes = [];
+
 function setup(){
     createCanvas(500,500);
     bird = new Bird();
+    pipes.push(new Pipe());
 }
 
 function draw(){
 
     background(0);
-
-    // ellipse(50, 300, 32,32);
-
     bird.show();
     bird.update();
+
+    if(frameCount % 100 == 0){
+        pipes.push(new Pipe());
+    }
+
+    for(i = 0; i < pipes.length; i++){
+        pipes[i].show();
+        pipes[i].update()    
+
+        if(pipes[i].hits(bird)){
+            console.log('hit')
+        }
+    }
     
 }
+
 function keyPressed(){
     if(key == ' '){
         bird.up()
@@ -51,6 +66,41 @@ function Bird(){
             this.y = 0;
             this.velocity = 0;
         }
+    }
+
+}
+
+function Pipe(){
+
+    this.top    = random(height/2);
+    this.bottom = random(height/2);
+    this.x = width;
+    this.w = 20;
+    this.speed = 2;
+    this.highlight = false;
+
+    this.hits = function(bird){
+
+        if(bird.y < this.top || bird.y > height - this.bottom){
+            if(bird.x > this.x && bird.x < this.x + this.w){
+                return true
+            }
+        }
+        this.highlight = false;
+        return false;
+    }
+
+    this.show = function(){
+        fill(255);
+        if(this.highlight){
+            fill(255, 0, 0)
+        }
+        rect(this.x, 0,this.w, this.top)
+        rect(this.x, height - this.bottom, this.w, this.bottom)
+    }
+
+    this.update = function(){
+        this.x  -= this.speed;
     }
 
 }
